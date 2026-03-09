3e4ada8cfafb7ddda2ae93fd457fd0710b5aa8d0
Giorgi Adikashvili <Giorgi.Adikashvili@wolterskluwer.com>
2026-03-09 14:25:23 +0400

    fix lecture 5

--- Lectures/Lecture5/src/main/java/food/Main.java
+++ Lectures/Lecture5/src/main/java/food/Main.java
@@ -2,15 +2,15 @@ package food;
 
 public class Main {
     public static void main(String[] args) {
-        eat();
+        Candy candy = new Candy();
+
+        eat(candy);
     }
 
-    public static void eat() {
-        Food candy = new Food();
+    public static void eat(Food food) {
+        Food broccoli = new Broccoli();
 
-        Food broccoli = new Candy();
-
-        if (candy.same(broccoli)) {
+        if (food.same(broccoli)) {
             System.out.println("broccoli!");
         }
     }

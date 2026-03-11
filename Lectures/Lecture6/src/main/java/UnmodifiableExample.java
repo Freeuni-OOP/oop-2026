3fdf1c02beee45cfdb1351239154e0587e89234f
Giorgi Adikashvili <Giorgi.Adikashvili@wolterskluwer.com>
2026-03-11 19:25:35 +0400

    add unmodifiable list

---
+++ Lectures/Lecture6/src/main/java/UnmodifiableExample.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class UnmodifiableExample {
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3);
+
+        List<Integer> unmodifiable = Collections.unmodifiableList(list);
+        unmodifiable.set(0, 10);
+    }
+}

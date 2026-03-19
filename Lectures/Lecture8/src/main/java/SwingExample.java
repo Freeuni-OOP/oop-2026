2a2c1699ac1e634a7c5f41f4a8681d1abee3f4f0
Giorgi Adikashvili <Giorgi.Adikashvili@wolterskluwer.com>
2026-03-19 12:09:50 +0400

    update swing

--- Lectures/Lecture8/src/main/java/SwingExample.java
+++ Lectures/Lecture8/src/main/java/SwingExample.java
@@ -1,7 +1,38 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SwingExample {
+
+    private static int count = 0;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(2, 1));
+
+        JButton button = new JButton("click");
+        JLabel label = new JLabel("count: 0");
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                count++;
+
+                label.setText("count: " + count);
+            }
+        });
+
+        panel.add(button);
+        panel.add(label);
+
+        frame.setContentPane(panel);
+        frame.setSize(400, 500);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
 
     private static class MyButton extends JButton {
         public MyButton(String text) {

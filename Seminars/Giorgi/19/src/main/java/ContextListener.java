6785817cddd7f1767d3a69eb4ba87c20d308c05d
Giorgi Adikashvili <Giorgi.Adikashvili@wolterskluwer.com>
2026-04-30 12:10:22 +0400

    seminar 19

--- Seminars/Giorgi/19/src/main/java/ContextListener.java
+++ Seminars/Giorgi/19/src/main/java/ContextListener.java
@@ -1,16 +1,36 @@
 import dao.ListStudentDAO;
+import dao.MySQLStudentDAO;
 import dao.StudentDAO;
+import org.apache.commons.dbcp2.BasicDataSource;
 import util.Constants;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 public class ContextListener implements ServletContextListener {
 
+    private BasicDataSource dataSource;
+
+    private static final String URL = "jdbc:mysql://localhost:3306/students_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        StudentDAO dao = new ListStudentDAO();
+        dataSource = new BasicDataSource();
+        dataSource.setUrl(URL);
+        dataSource.setUsername(USER);
+        dataSource.setPassword(PASSWORD);
+
+        StudentDAO dao;
+        try {
+            dao = new MySQLStudentDAO(dataSource);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
         ServletContext context = sce.getServletContext();
         context.setAttribute(Constants.DAO_KEY, dao);
@@ -18,6 +38,10 @@ public class ContextListener implements ServletContextListener {
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        // close database connection
+        try {
+            dataSource.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- Seminars/Giorgi/19/src/main/java/util/Constants.java
+++ Seminars/Giorgi/19/src/main/java/util/Constants.java
@@ -2,5 +2,7 @@ package util;
 
 public class Constants {
 
-    public static final String DAO_KEY= "DAO";
+    public static final String DAO_KEY = "DAO";
+
+    public static final String USER_NAME_COOKIE = "username";
 }

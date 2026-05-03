15d768313c040ec678a6cfc7214b4642d18bf975
Giorgi Adikashvili <Giorgi.Adikashvili@wolterskluwer.com>
2026-05-03 16:11:59 +0400

    lecture 20

---
+++ Lectures/Lecture20/src/test/java/ge/edu/freeuni/lecture20/util/GreetingServiceTest.java
@@ -0,0 +1,32 @@
+package ge.edu.freeuni.lecture20.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GreetingServiceTest {
+
+    private final GreetingService greetingService = new GreetingService();
+
+    @Test
+    public void testBuildGreetingUsesNameWhenProvided() {
+        String greeting = greetingService.buildGreeting("Nika");
+
+        assertEquals("Hello, Nika!", greeting);
+    }
+
+    @Test
+    public void testBuildGreetingReturnsGuestForBlankName() {
+        String greeting = greetingService.buildGreeting("   ");
+
+        assertEquals("Hello, Guest!", greeting);
+    }
+
+    @Test
+    public void testBuildGreetingReturnsGuestForNullName() {
+        String greeting = greetingService.buildGreeting(null);
+
+        assertEquals("Hello, Guest!", greeting);
+    }
+}
+

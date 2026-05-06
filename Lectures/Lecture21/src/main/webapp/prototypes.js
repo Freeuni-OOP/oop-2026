e8483ff1b1da4e4fa7a49628e897bc6663a52703
Giorgi Adikashvili <Giorgi.Adikashvili@wolterskluwer.com>
2026-05-06 17:15:13 +0400

    add prototypal inheritance

--- Lectures/Lecture21/src/main/webapp/prototypes.js
+++ Lectures/Lecture21/src/main/webapp/prototypes.js
@@ -3,3 +3,25 @@ export default function hello() {
 }
 
 hello.language = "English";
+
+// --------------- prototypal inheritance -----------------
+
+const vehicle = {
+    move() {
+        console.log("Vehicle is moving");
+    }
+};
+
+const car = Object.create(vehicle);
+car.wheels = 4;
+
+car.drive = function () {
+    console.log("Car is driving");
+};
+
+const tesla = Object.create(car);
+tesla.battery = 100;
+
+tesla.autoPilot = function () {
+    console.log("Autopilot enabled");
+};

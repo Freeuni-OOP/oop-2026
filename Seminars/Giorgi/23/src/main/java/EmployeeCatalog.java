396a3c848ae0f567b2b3c91a4445e2135ab20a98
Giorgi Adikashvili <Giorgi.Adikashvili@wolterskluwer.com>
2026-05-17 20:19:29 +0400

    seminar 23 default

---
+++ Seminars/Giorgi/23/src/main/java/EmployeeCatalog.java
@@ -0,0 +1,24 @@
+import javafx.util.Pair;
+
+import java.util.*;
+
+public class EmployeeCatalog {
+
+    private final List<Employee> employees;
+
+    public EmployeeCatalog(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public Employee findEmployeeWithHighestSalary() {
+        return null; // TODO
+    }
+
+    public int getSalaryGapInDepartment(Department department) {
+        return 0; // TODO
+    }
+
+    public Department findDepartmentWithHighestCumulativeSalary() {
+        return null; // TODO
+    }
+}
